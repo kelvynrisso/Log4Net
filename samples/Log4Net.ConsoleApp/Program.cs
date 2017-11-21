d35003a3772f41659815bb4512fe1936fa0bcd0e
Raju Joseph <raju.joseph@live.com>
2017-11-22 07:58:01 +0800

    consistent structure, artifacts, and packages

---
+++ samples/Log4Net.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Log4Net;
+
+
+namespace Log4Net.ConsoleApp {
+   public class Program {
+      public static void Main(string[] args) {
+         var serviceProvider = new ServiceCollection()
+            .AddLogging(builder => builder.AddLog4Net("log4net.config"))
+            .BuildServiceProvider();
+
+         using (var loggerFactory = serviceProvider.GetService<ILoggerFactory>()) {
+            var logger = loggerFactory.CreateLogger<Program>();
+
+            logger.LogTrace("This is a trace message");
+            logger.LogDebug("This is a debug message");
+            logger.LogInformation("This is an information message");
+            logger.LogWarning("This is a warning message");
+            logger.LogError("This is an warning error message");
+            logger.LogCritical("This is a critical message");
+         }
+
+         Console.WriteLine("Press [ENTER] to Exit!");
+         Console.ReadLine();
+      }
+   }
+}
